36a8905635c40a19af5a63fb0b8c0bdc7635066a
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-09-08 19:00:41 -0500

    finished task 2

--- index.js
+++ index.js
@@ -64,8 +64,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(array){
+ if(array.length <= 31){
+   return true
+ }else{
+   return false;
+ }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
